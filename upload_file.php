f042b69ad25fde78dc8bacd07b6ab3ddff086601
Burch Gürler <burchsan@gmail.com>
2014-05-28 17:29:53 +0200

    upload funkar minus y och pdf

--- upload_file.php
+++ upload_file.php
@@ -9,13 +9,18 @@ if ($_FILES["file"]["error"] > 0) {                     // den globala php array
 }
 ?>
 
-<?php                                                  // phpn sätter restriktioner
+<?php
 $allowedExts = array("gif", "jpeg", "jpg", "png");
 $temp = explode(".", $_FILES["file"]["name"]);
 $extension = end($temp);
 
-if ((($_FILES["file"]["type"] == "file/pdf"))
-&& ($_FILES["file"]["size"] < 8388608)  		// byt till övre gräns på 8 mb
+if ((($_FILES["file"]["type"] == "image/gif")
+|| ($_FILES["file"]["type"] == "image/jpeg")
+|| ($_FILES["file"]["type"] == "image/jpg")
+|| ($_FILES["file"]["type"] == "image/pjpeg")
+|| ($_FILES["file"]["type"] == "image/x-png")
+|| ($_FILES["file"]["type"] == "image/png"))
+&& ($_FILES["file"]["size"] < 8388608)
 && in_array($extension, $allowedExts)) {
   if ($_FILES["file"]["error"] > 0) {
     echo "Error: " . $_FILES["file"]["error"] . "<br>";
@@ -35,35 +40,30 @@ $allowedExts = array("gif", "jpeg", "jpg", "png");
 $temp = explode(".", $_FILES["file"]["name"]);
 $extension = end($temp);
 
-if ((($_FILES["file"]["type"] == "file/pdf") && ($_FILES["file"]["size"] < 8388608) && in_array($extension, $allowedExts)))
-{
-  if ($_FILES["file"]["error"] > 0) 
-  {
+if ((($_FILES["file"]["type"] == "image/gif")
+|| ($_FILES["file"]["type"] == "image/jpeg")
+|| ($_FILES["file"]["type"] == "image/jpg")
+|| ($_FILES["file"]["type"] == "image/pjpeg")
+|| ($_FILES["file"]["type"] == "image/x-png")
+|| ($_FILES["file"]["type"] == "image/png"))
+&& ($_FILES["file"]["size"] < 8388608)
+&& in_array($extension, $allowedExts)) {
+  if ($_FILES["file"]["error"] > 0) {
     echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
-  }  
-  else
-  {
+  } else {
     echo "Upload: " . $_FILES["file"]["name"] . "<br>";
     echo "Type: " . $_FILES["file"]["type"] . "<br>";
     echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
     echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
-    	if (file_exists("upload/" . $_FILES["file"]["name"]))
-    	{
-      		echo $_FILES["file"]["name"] . " already exists. ";
-    	}
-    		else 
-    		{
-      			move_uploaded_file($_FILES["file"]["tmp_name"],
-      			"Assets/upload/" . $_FILES["file"]["name"]);				// så den når mappen upload som jag creatat i C/wamp/www/assets/upload 
-      			echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
-    		}
-  	}
-		} 
-		else 
-		{
-  			echo "Invalid file";
-		}
+    if (file_exists("upload/" . $_FILES["file"]["name"])) {
+      echo $_FILES["file"]["name"] . " already exists. ";
+    } else {
+      move_uploaded_file($_FILES["file"]["tmp_name"],
+      "Assets/upload/" . $_FILES["file"]["name"]);
+      echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
+    }
+  }
+} else {
+  echo "Invalid file";
+}
 ?>
-
-
-
